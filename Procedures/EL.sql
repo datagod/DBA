36539a4fdc68a5ae2076a0eadbf568e12f6f3b91
datagod <william.mcevoy@gmail.com>
2023-05-10 16:37:16 -0400

    Update EL.sql

--- Procedures/EL.sql
+++ Procedures/EL.sql
@@ -1,13 +1,15 @@
-CREATE procedure [dbo].[EL]
+alter procedure [dbo].[EL]
 (
-  @Days decimal(5,2) = 0.01,
-  @Process varchar(100) = '%'
+  @Days        decimal(5,2) = 0.01,
+  @Process     varchar(100) = '%',
+  @Description varchar(100) = '%'
 )
 as
 
 
 select e1.EventLogID, 
        e1.EventTime, 
+      e1.databasename,
        'Seconds' = datediff(ss,e2.EventTime, e1.EventTime),
        'Process' = left(e1.Process,35),
        e1.Description
@@ -16,6 +18,7 @@ select e1.EventLogID,
  where e1.EventTime >= getdate()-(@Days)
    and e2.EventTime >= getdate()-(@Days)
    and e1.Process like @process
-   and e2.Process like @process
+   --and e2.Process like @process
+   and e1.Description like @description 
  order by e1.EventLogID asc
-
+go
