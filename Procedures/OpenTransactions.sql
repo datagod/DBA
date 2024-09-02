efd22ca9d679c79522714d7ff478a635a3a802ca
datagod <william.mcevoy@gmail.com>
2024-09-01 23:37:31 -0400

    Update OpenTransactions.sql

--- Procedures/OpenTransactions.sql
+++ Procedures/OpenTransactions.sql
@@ -1,21 +1,25 @@
-use dba
-go
-create or alter procedure OpenTransactions
-as
-select left(db_name(t.database_id),20) as 'Database',
-       left(s.Host_Name,15) as 'Host',
-       left(s.Login_name,25) as 'Login',
-       st.session_id,
-       Database_Transaction_Begin_Time as 'TxStart',
-       database_transaction_log_record_count as 'TxLogRecordCount',
-       database_transaction_log_bytes_used   as 'TxLogBytesUsed',
-       status
-       
-
-from sys.dm_tran_database_transactions t
-join sys.dm_tran_session_transactions st on t.transaction_id = st.transaction_id 
-join sys.dm_exec_sessions s on s.session_id = st.session_id 
-go
-
-dba.dbo.OpenTransactions
-
+CREATE or alter PROCEDURE OpenTransactions
+AS
+BEGIN
+    SELECT 
+        LEFT(DB_NAME(t.database_id), 20) AS 'Database',
+        LEFT(s.Host_Name, 15) AS 'Host',
+        LEFT(s.Login_name, 25) AS 'Login',
+        st.session_id,
+        t.Database_Transaction_Begin_Time AS 'TxStart',
+        t.database_transaction_log_record_count AS 'TxLogRecordCount',
+        t.database_transaction_log_bytes_used AS 'TxLogBytesUsed',
+        CASE 
+            WHEN DATEDIFF(SECOND, t.Database_Transaction_Begin_Time, GETDATE()) > 0 
+            THEN t.database_transaction_log_record_count / DATEDIFF(SECOND, t.Database_Transaction_Begin_Time, GETDATE())
+            ELSE 0 
+        END AS RecsPerSec,
+        s.status AS 'SessionStatus'
+    FROM 
+        sys.dm_tran_database_transactions t
+    JOIN 
+        sys.dm_tran_session_transactions st ON t.transaction_id = st.transaction_id 
+    JOIN 
+        sys.dm_exec_sessions s ON s.session_id = st.session_id;
+END
+ 
